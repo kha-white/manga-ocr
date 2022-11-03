af08ebadb6c6a09c6efa1119fe5f8bb7d0fc3ef2
Maciej Budyś <kha-white@mail.com>
2022-11-03 20:35:04 +0100

    add tests

---
+++ tests/generate_expected_results.py
@@ -0,0 +1,25 @@
+import json
+from pathlib import Path
+
+from tqdm import tqdm
+
+from manga_ocr import MangaOcr
+
+TEST_DATA_ROOT = Path(__file__).parent / 'data'
+
+
+def generate_expected_results():
+    mocr = MangaOcr()
+
+    results = []
+
+    for path in tqdm(sorted((TEST_DATA_ROOT / 'images').iterdir())):
+        result = mocr(path)
+        results.append({'filename': path.name, 'result': result})
+
+    (TEST_DATA_ROOT / 'expected_results.json').write_text(json.dumps(results, ensure_ascii=False, indent=2),
+                                                          encoding='utf-8')
+
+
+if __name__ == '__main__':
+    generate_expected_results()
---
+++ tests/test_ocr.py
@@ -0,0 +1,16 @@
+import json
+from pathlib import Path
+
+from manga_ocr import MangaOcr
+
+TEST_DATA_ROOT = Path(__file__).parent / 'data'
+
+
+def test_ocr():
+    mocr = MangaOcr()
+
+    expected_results = json.loads((TEST_DATA_ROOT / 'expected_results.json').read_text(encoding='utf-8'))
+
+    for item in expected_results:
+        result = mocr(TEST_DATA_ROOT / 'images' / item['filename'])
+        assert result == item['result']
